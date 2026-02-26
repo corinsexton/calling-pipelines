bd82c60ade3715df1ca44855b81e0b0881ad4e27
Michele Berselli <berselli.michele@gmail.com>
2026-02-26 15:52:49 -0500

    added support for indels Strelka file with a modified bcftools PASS filter

--- dockerfiles/calling_utils/bcftools_PASS_norm_dedup.sh
+++ dockerfiles/calling_utils/bcftools_PASS_norm_dedup.sh
@@ -11,9 +11,10 @@ IFS=$'\n\t'
 
 usage() {
   cat <<EOF
-Usage: $0 -i input.vcf.gz -r reference.fasta [-o prefix]
+Usage: $0 -i input.vcf.gz -f additional.vcf.gz [-f additional.vcf.gz ...] -r reference.fasta [-o prefix]
 
   -i   Input VCF (bgzipped) with .tbi index (required)
+  -f   Additional input VCF file to merge to main input(bgzipped with .tbi index, optional, can be specified multiple times)
   -r   Reference FASTA with .fai index (required)
   -o   Prefix for the output annotated VCF (default: output)
        The final output will be <prefix>.vcf.gz
@@ -24,15 +25,17 @@ EOF
 
 # Default
 INPUT_VCF=""
+ADDITIONAL_VCFS=()
 REFERENCE_FASTA=""
 OUTPUT_PRFX="output"
 # Define threads
 NTHREADS=$(nproc)
 
 # Parse args
-while getopts "i:r:o:h" opt; do
+while getopts "i:f:r:o:h" opt; do
   case $opt in
     i) INPUT_VCF="$OPTARG" ;;
+    f) ADDITIONAL_VCFS+=("$OPTARG") ;;
     r) REFERENCE_FASTA="$OPTARG" ;;
     o) OUTPUT_PRFX="$OPTARG" ;;
     h) usage ;;
@@ -53,10 +56,40 @@ command -v bcftools >/dev/null 2>&1 || { echo "Error: bcftools not found in PATH
 [[ -f "$REFERENCE_FASTA" ]] || { echo "Error: $REFERENCE_FASTA not found"; exit 1; }
 [[ -f "${REFERENCE_FASTA}.fai" ]] || { echo "Error: ${REFERENCE_FASTA}.fai not found"; exit 1; }
 
+if (( ${#ADDITIONAL_VCFS[@]} > 0 )); then
+    for vcf in "${ADDITIONAL_VCFS[@]}"; do
+        [[ -f "$vcf" ]] || { echo "Error: additional VCF file not found: $vcf"; exit 1; }
+        [[ -f "${vcf}.tbi" ]] || { echo "Error: index for additional VCF file not found: ${vcf}.tbi"; exit 1; }
+    done
+fi
+
+# Cleanup function to remove temporary files on exit
+TMP_FILES=()
+
+cleanup() {
+    rm -f "${TMP_FILES[@]}"
+}
+trap cleanup EXIT
+
+# Concatenating additional VCF files if provided
+if (( ${#ADDITIONAL_VCFS[@]} > 0 )); then
+    echo "Merging additional VCF files..."
+    ALL_VCFS=("$INPUT_VCF" "${ADDITIONAL_VCFS[@]}")
+    MERGED_VCF=$(mktemp -u ./MERGED_XXXXXX.vcf.gz)
+    TMP_FILES+=("$MERGED_VCF" "$MERGED_VCF.tbi")
+
+    bcftools concat -a -Ou "${ALL_VCFS[@]}" \
+    | bcftools sort -T "tmp_bcftools.XXXXXX" -Oz -o "$MERGED_VCF" \
+    || { echo "Error: bcftools concat/sort step failed"; exit 1; }
+
+    bcftools index --threads "$NTHREADS" --tbi "$MERGED_VCF" || { echo "Error: bcftools index failed for merged VCF"; exit 1; }
+    INPUT_VCF="$MERGED_VCF"
+fi
+
 # Check header to set the filter correctly
 TMP_HEAD="$(mktemp ./HEAD_XXXXXX)"
+TMP_FILES+=("$TMP_HEAD")
 bcftools view --header-only "$INPUT_VCF" > "$TMP_HEAD"
-trap 'rm -f "$TMP_HEAD"' EXIT
 
 # Set filter based on header
 if grep -q -m1 'ID=FEX' "$TMP_HEAD"; then
